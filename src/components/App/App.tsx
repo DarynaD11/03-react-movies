8d856b5defca771d93174f3de7f24edbb6a8b93f
Daryna <darinadyadyuk11@gmail.com>
2025-07-22 21:25:37 +0300

    done

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,8 +1,59 @@
-// import { useState } from "react";
-import "./App.module.css";
+import css from "./App.module.css";
+import SearchBar from "../SearchBar/SearchBar";
+import MovieGrid from "../MovieGrid/MovieGrid";
+import MovieModal from "../MovieModal/MovieModal";
+import Loader from "../Loader/Loader";
+import ErrorMessage from "../ErrorMessage/ErrorMessage";
+import { fetchMovies } from "../../services/movieService";
+import type { Movie } from "../../types/movie";
+import { useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
 
-function App() {
-  return <></>;
+export default function App() {
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const handleSearch = async (query: string) => {
+    setLoading(true);
+    setError(false);
+    setMovies([]);
+
+    try {
+      const results = await fetchMovies(query);
+      if (results.length === 0) {
+        toast.error("No movies found for your request.");
+      }
+      setMovies(results);
+      setError(false);
+    } catch {
+      setError(true);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleSelectMovie = (movie: Movie) => {
+    setSelectedMovie(movie);
+  };
+
+  const handleCloseModal = () => {
+    setSelectedMovie(null);
+  };
+
+  return (
+    <div className={css.app}>
+      <Toaster />
+      <SearchBar onSubmit={handleSearch} />
+      {loading && <Loader />}
+      {error && <ErrorMessage />}
+      {!loading && !error && (
+        <MovieGrid movies={movies} onSelect={handleSelectMovie} />
+      )}
+      {selectedMovie && (
+        <MovieModal movie={selectedMovie} onClose={handleCloseModal} />
+      )}
+    </div>
+  );
 }
-
-export default App;
